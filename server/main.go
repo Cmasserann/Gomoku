29ee4b2294b5d1fb3e34173d44e2ec6bf7b3f9b5
Charles Masserann <c.masserann@proton.me>
2025-12-19 11:25:55 +0100

    🚀 Time to merge API

--- server/main.go
+++ server/main.go
@@ -17,6 +17,10 @@ type MoveRequest struct {
 	Color int `json:"color"` // 1 = Noir, 2 = Blanc
 }
 
+type DebugRequest struct {
+	SplitedGoban [][]int `json:"board"`
+}
+
 
 func updateGoban(goban []byte,x int, y int, color int) bool {
 
@@ -24,7 +28,7 @@ func updateGoban(goban []byte,x int, y int, color int) bool {
 
 	fmt.Println("Index calculé :", index)
 
-	if index > -1 && index < gobanSize && (color == 1 || color == 2) && goban[index] == 0 {
+	if index > -1 && index < gobanSize && color > -1 && color < 3 {
 		goban[index] = byte(color)
 		return true
 	}
@@ -32,7 +36,27 @@ func updateGoban(goban []byte,x int, y int, color int) bool {
 	return false
 }
 
+func setGoban(goban []byte, splitedGoban [][]int) bool {
+
+	if len(splitedGoban) != gobanWidth {
+		return false
+	}
+
+	for i := 0; i < gobanWidth; i++ {
+		if len(splitedGoban[i]) != gobanWidth {
+			return false
+		}
+		for j := 0; j < gobanWidth; j++ {
+			index := i*gobanWidth + j
+			goban[index] = byte(splitedGoban[i][j])
+		}
+	}
+
+	return true
+}
+
 func printGoban(goban []byte) {
+
 	fmt.Println("Goban actuel :")
 	for i := 0; i < gobanWidth; i++ {
 		for j := 0; j < gobanWidth; j++ {
@@ -44,6 +68,7 @@ func printGoban(goban []byte) {
 }
 
 func convertGobanTo2D(goban []byte) [][]int {
+
 	board2D := make([][]int, gobanWidth)
 	for i := range board2D {
 		board2D[i] = make([]int, gobanWidth)
@@ -60,8 +85,6 @@ func convertGobanTo2D(goban []byte) [][]int {
 func main() {
 
 	goban := make([]byte, gobanSize)
-
-	printGoban(goban)
 
 	r := gin.Default()
 
@@ -101,6 +124,33 @@ func main() {
 
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Coup accepté",
+			"board":   convertGobanTo2D(goban),
+		})
+	})
+
+	r.POST("/debug", func(c *gin.Context) {
+		var req DebugRequest
+
+
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Donnée invalide (besoin de splited_goban)"})
+			return
+		}
+
+		newGoban := req.SplitedGoban
+
+		fmt.Println("Nouveau goban reçu :")
+		for i := 0; i < gobanWidth; i++ {
+			for j := 0; j < gobanWidth; j++ {
+				fmt.Printf("%d ", newGoban[i][j])
+			}
+			fmt.Println()
+		}
+
+		setGoban(goban, newGoban)
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Goban mis à jour",
 			"board":   convertGobanTo2D(goban),
 		})
 	})
