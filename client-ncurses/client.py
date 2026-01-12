42bbbf09598393811c0bfb2fef5073810359c423
Raphael Vinour <raphael.vinour@gmail.fr>
2026-01-12 15:58:33 +0100

    🎨 add big_goban in ncusre client

--- client-ncurses/client.py
+++ client-ncurses/client.py
@@ -9,10 +9,11 @@ y_input = -1
 sug_x = -1
 sug_y = -1
 
+
 def draw_menu(stdscr: curses.window):
     key = 0
     selection = 0
-    stdscr.clear()    
+    stdscr.clear()
     stdscr.refresh()
 
     curses.curs_set(0)
@@ -20,25 +21,35 @@ def draw_menu(stdscr: curses.window):
     curses.mousemask(curses.ALL_MOUSE_EVENTS)
 
     curses.start_color()
+    # base color
     curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    # suggestion color
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_RED)
+    # cursor selection color
     curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    # cursor on top of suggestion
     curses.init_pair(4, curses.COLOR_RED, curses.COLOR_WHITE)
+    # opponents color
+    curses.init_pair(5, curses.COLOR_RED, curses.COLOR_BLACK)
+    # oppoenents color cursor
+    curses.init_pair(6, curses.COLOR_RED, curses.COLOR_WHITE)
+    # your color cursor
+    curses.init_pair(7, curses.COLOR_CYAN, curses.COLOR_WHITE)
 
     title = "Welcome to the Go Game!"
     quit_msg = "Quit. [q]"
     play_msg = "Play the game. [p]"
 
-    while (key != ord('q')):
+    while key != ord("q"):
         stdscr.clear()
         height, width = stdscr.getmaxyx()
 
-        if key == curses.KEY_DOWN or key == ord('s'):
+        if key == curses.KEY_DOWN or key == ord("s"):
             selection = (selection + 1) % 2
-        elif key == curses.KEY_UP or key == ord('w'):
+        elif key == curses.KEY_UP or key == ord("w"):
             selection = (selection - 1) % 2
 
-        if key == ord('\n'):
+        if key == ord("\n"):
             if selection == 0:
                 draw_game(stdscr)
             else:
@@ -51,12 +62,19 @@ def draw_menu(stdscr: curses.window):
         x_quit = int((width // 2) - (len(quit_msg) // 2) - len(quit_msg) % 2)
         y_quit = y_play + 2
 
-        if x_title < 0 or x_play < 0 or x_quit < 0 or y_title < 0 or y_play <= 2 or y_quit <= 4:
+        if (
+            x_title < 0
+            or x_play < 0
+            or x_quit < 0
+            or y_title < 0
+            or y_play <= 2
+            or y_quit <= 4
+        ):
             stdscr.addstr(0, 0, "Terminal too small!", curses.color_pair(2))
             stdscr.refresh()
             key = stdscr.getch()
             continue
-    
+
         stdscr.addstr(y_title, x_title, title)
 
         if selection == 0:
@@ -74,8 +92,9 @@ def draw_menu(stdscr: curses.window):
 
         key = stdscr.getch()
 
-        if key == ord('p'):
+        if key == ord("p"):
             draw_game(stdscr)
+
 
 def draw_game(stdscr: curses.window):
     global turn_to_play
@@ -88,6 +107,7 @@ def draw_game(stdscr: curses.window):
     key = 0
     cursor_x = 0
     cursor_y = 0
+    big_goban = True
 
     board = client_tool.get_board()
     if not board:
@@ -98,15 +118,23 @@ def draw_game(stdscr: curses.window):
         stdscr.refresh()
         return
 
-    while (key != ord('q')):
+    while key != ord("q"):
         stdscr.clear()
         height, width = stdscr.getmaxyx()
 
-        if height < len(board["board"]) + 10     or width < len(board["board"]) * 2:
+        if height < len(board["board"]) + 5 or width < len(board["board"]) * 2:
             stdscr.addstr(0, 0, "Terminal too small!", curses.color_pair(2))
             stdscr.refresh()
             key = stdscr.getch()
             continue
+
+        if (
+            width >= len(board["board"]) * 4 + 2
+            and height >= len(board["board"]) * 2 + 5
+        ):
+            big_goban = True
+        else:
+            big_goban = False
 
         if not turn_to_play:
             new_board = client_tool.wait_for_change(board)
@@ -114,95 +142,83 @@ def draw_game(stdscr: curses.window):
                 board = new_board
                 turn_to_play = True
 
-        goban = board["board"]
+        goban: list[list[str]] = board["board"]
 
-        for y in range(len(goban)):
-            for x in range(len(goban[y])):
-                char = "."
-                if (x, y) == (cursor_x, cursor_y):
-                    stdscr.attron(curses.color_pair(3))
-                if (x, y) == (sug_x, sug_y):
-                    if (x, y) == (cursor_x, cursor_y):
-                        stdscr.attron(curses.color_pair(4))
-                    else:
-                        stdscr.attron(curses.color_pair(2))
-                if goban[y][x] == 1:
-                    char = "B"
-                elif goban[y][x] == 2:
-                    char = "W"
-                stdscr.addstr(y, x * 2, char)
-                stdscr.attroff(curses.color_pair(3))
-        
+        if big_goban:
+            draw_big_goban(stdscr, goban, cursor_x, cursor_y, width)
+        else:
+            draw_goban(stdscr, goban, cursor_x, cursor_y, width)
+
         stdscr.attron(curses.color_pair(3))
         stdscr.addstr(height - 1, 0, "Press 'q' to exit.")
         stdscr.attroff(curses.color_pair(3))
 
-        if key == curses.KEY_UP:
-            cursor_y = max(0, cursor_y - 1)
-        elif key == curses.KEY_DOWN:
-            cursor_y = min(len(goban) - 1, cursor_y + 1)
-        elif key == curses.KEY_LEFT:
-            cursor_x = max(0, cursor_x - 1)
-        elif key == curses.KEY_RIGHT:
-            cursor_x = min(len(goban) - 1, cursor_x + 1)
+        cursor_x, cursor_y = client_tool.get_cursor_pos(
+            key, cursor_x, cursor_y, len(goban)
+        )
 
-        if key == curses.KEY_MOUSE :
+        if key == curses.KEY_MOUSE:
             _, mx, my, _, _ = curses.getmouse()
-            grid_x = mx // 2
-            grid_y = my
+            if big_goban:
+                grid_x = mx // 4
+                grid_y = (my - 1) // 2
+            else:
+                grid_x = mx // 2
+                grid_y = my
             if grid_x < len(goban) and grid_y < len(goban):
+                cursor_x = grid_x
+                cursor_y = grid_y
                 send_move(grid_x, grid_y, 1)
 
-        stdscr.addstr(len(goban) + 2, 0, f"Cursor at x={cursor_x}, y={cursor_y}", curses.color_pair(1))
+        start_line = len(goban) * 2 if big_goban else len(goban)
+        draw_info_panel(stdscr, start_line  , turn_to_play, big_goban)
 
-        if turn_to_play:
-            stdscr.addstr(len(goban) + 4, 0, "Your turn to play.", curses.color_pair(1))
-        else:
-            stdscr.addstr(len(goban) + 4, 0, "Waiting for opponent...", curses.color_pair(1))
-
-        if key in range(ord('0'), ord('9') + 1):
-            digit = key - ord('0')
-            if not space_pressed :
-                if x_input == -1 :
+        if key in range(ord("0"), ord("9") + 1):
+            digit = key - ord("0")
+            if not space_pressed:
+                if x_input == -1:
                     x_input = digit
-                else :
+                else:
                     x_input = x_input * 10 + digit
-                    if x_input >= len(goban) :
+                    if x_input >= len(goban):
                         x_input = -1
-            else :
-                if y_input == -1 :
+            else:
+                if y_input == -1:
                     y_input = digit
-                else :
+                else:
                     y_input = y_input * 10 + digit
-                    if y_input >= len(goban) :
+                    if y_input >= len(goban):
                         y_input = -1
-        
-        if not space_pressed and key == ord(' ') and x_input != -1 :
+
+        if not space_pressed and key == ord(" ") and x_input != -1:
             space_pressed = True
-        elif key == ord('\n') and turn_to_play :
-            if space_pressed and y_input != -1 :
+        elif space_pressed and key == ord(" "):
+            space_pressed = False
+            y_input = -1
+            x_input = -1
+        elif key == ord("\n") and turn_to_play:
+            if space_pressed and y_input != -1:
                 send_move(x_input, y_input, 1)
-            elif cursor_x != -1 and cursor_y != -1 :
+            elif cursor_x != -1 and cursor_y != -1:
                 send_move(cursor_x, cursor_y, 1)
 
-        if x_input != -1 :
-            stdscr.addstr(len(goban) + 5, 0, f"X input: {x_input}", curses.color_pair(1))
-        if y_input != -1 :
-            stdscr.addstr(len(goban) + 6, 0, f"Y input: {y_input}", curses.color_pair(1))
-
-        if key == ord('c') :
+        if key == ord("c"):
             x_input = -1
             y_input = -1
             space_pressed = False
 
-        if key == ord('h') :
+        if key == ord("h"):
             ret = client_tool.ai_suggest()
-            if ret :
+            if ret:
                 sug_x = ret.get("x", -1)
                 sug_y = ret.get("y", -1)
-        if sug_x != -1 and sug_y != -1 :
-            stdscr.addstr(len(goban) + 7, 0, f"AI suggests move at x={sug_x}, y={sug_y}", curses.color_pair(1))
-
+        if sug_x != -1 and sug_y != -1:
+            stdscr.addstr(
+                start_line + 7,
+                0,
+                f"AI suggests move at x={sug_x}, y={sug_y}",
+                curses.color_pair(1),
+            )
 
         stdscr.refresh()
 
@@ -213,10 +229,147 @@ def draw_game(stdscr: curses.window):
     stdscr.clear()
     stdscr.refresh()
 
+
+def draw_goban(
+    stdscr: curses.window,
+    goban: list[list[int]],
+    cursor_x: int,
+    cursor_y: int,
+    width: int,
+):
+    start_y = int((width // 2) - (len(goban) // 2) - len(goban) % 2)
+    start_y = 0
+
+    for y in range(len(goban)):
+        for x in range(len(goban[y])):
+            char = "."
+            if (x, y) == (cursor_x, cursor_y):
+                stdscr.attron(curses.color_pair(3))
+            if (x, y) == (sug_x, sug_y):
+                if (x, y) == (cursor_x, cursor_y):
+                    stdscr.attron(curses.color_pair(4))
+                else:
+                    stdscr.attron(curses.color_pair(2))
+            if goban[y][x] == 1:
+                char = "B"
+            elif goban[y][x] == 2:
+                char = "W"
+            stdscr.addstr(int(start_y + y), x * 2, char)
+            stdscr.attroff(curses.color_pair(3))
+
+
+def draw_big_goban(
+    stdscr: curses.window,
+    goban: list[list[int]],
+    cursor_x: int,
+    cursor_y: int,
+    width: int,
+):
+    start_y = int((width // 2) - (len(goban * 2) // 2) - len(goban) % 2)
+    start_y = 0
+
+    for y in range(len(goban)):
+        stdscr.addstr(y * 2, 0, "║")
+        stdscr.addstr(y * 2 + 1, 0, "║")
+        stdscr.addstr(y * 2, len(goban[y] * 4) + 1, "║")
+        stdscr.addstr(y * 2 + 1, len(goban[y] * 4) + 1, "║")
+        if y == 0:
+            stdscr.addstr(y, 0, "╔" + "════" * (len(goban[y]) - 1) + "════╗")
+        if y == len(goban) - 1:
+            stdscr.addstr(y * 2 + 2, 0, "║")
+            stdscr.addstr(y * 2 + 2, len(goban[y] * 4) + 1, "║")
+            stdscr.addstr(y * 2 + 3, 0, "╠" + "════" * (len(goban[y]) - 1) + "════╝")
+        for x in range(len(goban[y])):
+            char: list[str] = ["  ", "__"]
+            if goban[y][x] == 1:
+                char = ["╔╗", "╚╝"]
+            elif goban[y][x] == 2:
+                char = ["┌┐", "└┘"]
+
+            color = select_color(x, y, cursor_x, cursor_y, goban[y][x])
+            stdscr.attron(curses.color_pair(color))
+
+            # stdscr.addstr(y * 2 + 1, x * 4 + 2, char[0])
+            # stdscr.addstr(y * 2 + 2, x * 4 + 2, char[1])
+            stdscr.addstr(start_y + y * 2 + 1, x * 4 + 2, char[0])
+            stdscr.addstr(start_y + y * 2 + 2, x * 4 + 2, char[1])
+
+            stdscr.attroff(curses.color_pair(3))
+
+def draw_info_panel(stdscr: curses.window, start_line: int, turn_to_play: bool, big_goban: bool):
+    if turn_to_play:
+        msg = " Your turn to play. "
+    else:
+        msg = " Waiting for opponent... "
+    capt_msg_1 = " B Capture: " + "1 "
+    capt_msg_2 = " W Capture: " + "0 "
+
+    if big_goban:
+        start_line += 2
+        stdscr.addstr(start_line, 0, "║")
+        stdscr.addstr(start_line + 1, 0, "╚")
+
+        stdscr.attron(curses.color_pair(1))
+        stdscr.addstr(start_line, 0 + 1, msg)
+        stdscr.attroff(curses.color_pair(1))
+        stdscr.addstr(start_line + 1, 0 + 1, "═" * (len(msg) + len(capt_msg_1) + len(capt_msg_2) + 2))
+
+        stdscr.addstr(start_line, len(msg) + 1, "║")
+        stdscr.addstr(start_line - 1, len(msg) + 1, "╦")
+        stdscr.addstr(start_line + 1, len(msg) + 1, "╩")
+
+        stdscr.attron(curses.color_pair(1))
+        stdscr.addstr(start_line, len(msg) + 2, capt_msg_1)
+        stdscr.attron(curses.color_pair(5))
+        stdscr.addstr(start_line, len(msg) + len(capt_msg_1) + 3, capt_msg_2)
+        stdscr.attroff(curses.color_pair(5))
+
+        stdscr.addstr(start_line - 1, len(msg) + len(capt_msg_1) + 2, "╦")
+        stdscr.addstr(start_line, len(msg) + len(capt_msg_1) + 2, "║")
+        stdscr.addstr(start_line + 1, len(msg) + len(capt_msg_1) + 2, "╩")
+        stdscr.addstr(start_line - 1, len(msg) + len(capt_msg_1) + len(capt_msg_2) + 3, "╦")
+        stdscr.addstr(start_line, len(msg) + len(capt_msg_1) + len(capt_msg_2) + 3, "║")
+        stdscr.addstr(start_line + 1, len(msg) + len(capt_msg_1) + len(capt_msg_2) + 3, "╝")
+    else:
+        
+        stdscr.addstr(start_line , 0, msg)
+
+        stdscr.attron(curses.color_pair(1))
+        stdscr.addstr(start_line + 2, 0, capt_msg_1)
+        stdscr.attroff(curses.color_pair(1))
+
+        stdscr.attron(curses.color_pair(5))
+        stdscr.addstr(start_line + 3, 0, capt_msg_2)
+        stdscr.attroff(curses.color_pair(5))
+
+
+
+def select_color(x: int, y: int, cursor_x: int, cursor_y: int, value: int) -> int:
+    global sug_x
+    global sug_y
+
+    if (x, y) == (sug_x, sug_y):
+        if (x, y) == (cursor_x, cursor_y):
+            return 4
+        return 2
+    elif value == 0:
+        if (x, y) == (cursor_x, cursor_y):
+            return 3
+    elif value == 1:
+        if (x, y) == (cursor_x, cursor_y):
+            return 7
+        return 1
+    elif value == 2:
+        if (x, y) == (cursor_x, cursor_y):
+            return 4
+        return 5
+    return 0
+
+
 def send_move(x: int, y: int, color: int):
     resp = client_tool.send_move(x, y, color)
 
-    global x_input 
+    global x_input
     global y_input
     global sug_x
     global sug_y
@@ -227,12 +380,14 @@ def send_move(x: int, y: int, color: int):
     y_input = -1
     sug_x = -1
     sug_y = -1
-    if resp :
+    if resp:
         space_pressed = False
         turn_to_play = False
 
+
 def main():
     curses.wrapper(draw_menu)
+
 
 if __name__ == "__main__":
     main()
--- client-ncurses/client_tool.py
+++ client-ncurses/client_tool.py
@@ -1,11 +1,12 @@
+import curses
 from typing import Any
-import requests # type: ignore
+import requests  # type: ignore
 import time
 
 URL_BASE = "http://127.0.0.1:8080"
 
-def get_board() -> dict[str, Any]:
 
+def get_board() -> dict[str, Any]:
     try:
         response = requests.get(f"{URL_BASE}/board")
         return response.json()
@@ -13,8 +14,8 @@ def get_board() -> dict[str, Any]:
         print(f"Connection fail : {e}")
         return dict()
 
-def send_move(x: int, y: int, color: int) -> (dict[str, Any] | None):
 
+def send_move(x: int, y: int, color: int) -> dict[str, Any] | None:
     payload = {"x": x, "y": y, "color": color}
     try:
         response = requests.post(f"{URL_BASE}/move", json=payload)
@@ -24,15 +25,17 @@ def send_move(x: int, y: int, color: int) -> (dict[str, Any] | None):
     except Exception as e:
         print(f"Error : {e}")
         return None
-    
+
+
 def wait_for_change(old_board: dict[str, Any]):
     while True:
         new_board = get_board()
-        if new_board['board'] != old_board['board']:
+        if new_board["board"] != old_board["board"]:
             return new_board
         time.sleep(0.2)
 
-def ai_suggest() -> (dict[str, int] | None):
+
+def ai_suggest() -> dict[str, int] | None:
     try:
         response = requests.get(f"{URL_BASE}/ai-suggest")
         if response.status_code != 200:
@@ -41,3 +44,17 @@ def ai_suggest() -> (dict[str, int] | None):
     except Exception as e:
         print(f"Error : {e}")
         return None
+
+
+def get_cursor_pos(
+    key: int, cursor_x: int, cursor_y: int, goban_size: int
+) -> tuple[int, int]:
+    if key == curses.KEY_UP:
+        cursor_y = max(0, cursor_y - 1)
+    elif key == curses.KEY_DOWN:
+        cursor_y = min(goban_size - 1, cursor_y + 1)
+    elif key == curses.KEY_LEFT:
+        cursor_x = max(0, cursor_x - 1)
+    elif key == curses.KEY_RIGHT:
+        cursor_x = min(goban_size - 1, cursor_x + 1)
+    return cursor_x, cursor_y
